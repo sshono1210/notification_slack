8549803f038109ec6b37275a43446d0b17ac7ea2
shono <s.shosho1210@gmail.com>
2017-11-14 00:44:00 +0900

    connect route

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/dog', function () {
+    return view('dog');
+});
+Route::post('/dog', function () {
+    return redirect('/dog');
 });
